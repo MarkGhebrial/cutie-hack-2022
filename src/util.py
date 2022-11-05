3300ce2b004ed83b53135c5f4e25bf06cb2470a9
Charlie K <crknox412@gmail.com>
2022-11-05 13:39:48 -0700

    Defined bounding_box function in util

--- src/util.py
+++ src/util.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageFont
 import math
+from main import CHARACTERS
 
 def chunk_image(im: Image, charProportions, columns = 50):
     chunkWidth = math.ceil(im.width / columns)
@@ -13,7 +14,21 @@ def chunk_image(im: Image, charProportions, columns = 50):
             yield out
 
 def bounding_box(font: ImageFont, s: str): # Returns a tuple of (width, height):
-    pass
+    fontName = font
+    
+    fnt = ImageFont.truetype(fontName, 15)
+
+    (left, top, right, bottom) = fnt.getbbox(CHARACTERS)
+
+    height = bottom + top
+
+    txt = s
+
+    (left, top, right, bottom) = font.getbbox(txt)
+
+    width = right - left
+
+    return (width, height)
 
 def char_to_string(font: ImageFont, c: str): # Returns an Image
     pass
