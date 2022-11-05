89d935de0625c9fd5fffddd74ba5e7e6e84a372d
MarkGhebrial <mghebrialjr@icloud.com>
2022-11-05 13:50:04 -0700

    Implement char_to_img

--- src/util.py
+++ src/util.py
@@ -1,9 +1,39 @@
-from PIL import Image, ImageFont
+from PIL import Image, ImageFont, ImageDraw
 import math
+from main import CHARACTERS
 
-def chunk_image(im: Image, charProportions, columns = 50):
+def bounding_box(font: ImageFont, s: str): # Returns a tuple of (width, height):
+    pass
+
+def char_to_img(font: ImageFont, c: str): # Returns an Image
+    (w, h) = bounding_box(c)
+
+    # Create an image that's the same size as the chracter
+    img = Image.new("RGB", (w, h), (0, 0, 0))
+
+    d = ImageDraw.Draw(img)
+
+    d.text((0, 0), c, font=font, fill=(255, 255, 255))
+
+def font_proportions(font: ImageFont):
+    '''
+    Returns the height/width ratio of characters in the specified font.
+    If the font is not monospace (i.e. the width/height of each
+    character is different), then `None` is returned
+    '''
+
+    boundingBox = bounding_box(font, "a")
+    for c in CHARACTERS:
+        if bounding_box(font, c) != boundingBox:
+            return None
+    return boundingBox[1] / boundingBox[0]
+
+
+def chunk_image(im: Image, font: ImageFont, columns = 50):
+    proportions = font_proportions(font)
+
     chunkWidth = math.ceil(im.width / columns)
-    chunkHeight = chunkWidth * charProportions
+    chunkHeight = chunkWidth * proportions
 
     for x in range(columns):
         y = 0
@@ -11,9 +41,3 @@ def chunk_image(im: Image, charProportions, columns = 50):
             out = im.crop((x * chunkWidth, y, (x+1) * chunkWidth, y + chunkHeight))
             y += int(chunkHeight)
             yield out
-
-def bounding_box(font: ImageFont, s: str): # Returns a tuple of (width, height):
-    pass
-
-def char_to_string(font: ImageFont, c: str): # Returns an Image
-    pass
