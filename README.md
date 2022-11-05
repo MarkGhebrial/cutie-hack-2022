e38dde99d6c7e0a65ebe86a8a47b35555921ab2b
MarkGhebrial <mghebrialjr@icloud.com>
2022-11-05 10:26:02 -0700

    Parse command line arguments

--- README.md
+++ README.md
@@ -5,3 +5,20 @@
 Run `pip install -r requirements.txt`
 
 TODO
+
+# Usage
+
+```
+usage: main.py [-h] [-f FONTNAME] [-o OUTNAME] imageName
+
+Convert an image to ASCII art.
+
+positional arguments:
+  imageName             The path of the image to convert
+
+options:
+  -h, --help            show this help message and exit
+  -f FONTNAME, --font FONTNAME
+                        Which system font to use
+  -o OUTNAME            What file to output the resulting art to
+```
--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description="Convert an image to ASCII art.")
+
+    parser.add_argument(
+        "imageName",
+        help="The path of the image to convert"
+    )
+    parser.add_argument(
+        "-f", "--font",
+        dest="fontName",
+        help="Which system font to use"
+    )
+    parser.add_argument("-o",
+        dest="outName",
+        help="What file to output the resulting art to"
+    )
+
+    args = parser.parse_args()
+
+    print(args.imageName)
+
     print("Hello world")
 
 
