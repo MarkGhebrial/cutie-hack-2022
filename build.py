f708a7f37fcb23edbd5c9717e9e7c2aaeeaece40
Mark Ghebrial <mghebrialjr@icloud.com>
2022-11-06 11:01:51 -0800

    Specify module versions in requirements.txt

--- build.py
+++ build.py
@@ -1,8 +1,23 @@
+import platform
 import PyInstaller.__main__
 
-PyInstaller.__main__.run([
-    "src/main.py",
-    "--onefile",
-    "--add-data", "fonts;fonts",
-    "--name", "imascii"
-])
+system = platform.system()
+
+if system == "Linux" or system == "Darwin":
+    PyInstaller.__main__.run([
+        "src/main.py",
+        "--onefile",
+        "--add-data", "fonts:fonts",
+        "--name", "imascii"
+    ])
+
+elif system == "Windows":
+    PyInstaller.__main__.run([
+        "src/main.py",
+        "--onefile",
+        "--add-data", "fonts;fonts",
+        "--name", "imascii"
+    ])
+
+else:
+    print("System \"" + system + "\" not supported")
